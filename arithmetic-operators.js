8153966ea4d56118aadb629a7d22bb5904269e13
Marcio <marciosoaresthoman@gmail.com>
2022-02-06 03:34:58 -0300

    Organizando/4.1

---
+++ arithmetic-operators.js
@@ -0,0 +1,37 @@
+let num1 = 5; //Number
+let num2 = 10;
+let num3 = 15;
+
+
+// // JS é uma linguagem de tipagem fraca.
+
+// // comentário aqui
+
+
+// // Soma +
+// // let sum = num1 + num2; // 5 + 10
+// // console.log(sum);
+
+// // Subtração -
+// let subtraction = num1 - num2;
+// console.log(subtraction);
+
+// // Divisão
+// // let division = num1 / num2;
+// // console.log(division);
+
+// // // // Multiplicação *
+// // let multiplication = num1 * num2;
+// // console.log(multiplication);
+
+// // Como saber se o número é par?
+// // % -> operador de resto
+// ((num1 + num2) % 2) === 0;
+// ((5 + 10) % 2) === 0;
+// 15 % 2 === 0; //15 é um número ímpar
+
+// // Operador de relação
+let comparison = num1 > num2; //boolean
+let comparison = 5 == 10; //boolean
+console.log(comparison);
+
---
+++ switch.js
@@ -0,0 +1,41 @@
+let weekDay = 3;
+
+switch (weekDay) {
+  case 1:
+    console.log('Domingo');
+    break;
+  case 2:
+    console.log('Segunda-feira');
+    break;
+  case 3:
+    console.log('Terça-feira');
+    break;
+  case 4:
+    console.log('Quarta-feira');
+    break;
+  case 5:
+    console.log('Quinta-feira');
+    break;
+  case 6:
+    console.log('Dia de maldade');
+    break;
+  case 7:
+    console.log('Sábado');
+    break;
+  default:
+    console.log('Não existe esse dia, ainda');
+    break;
+}
+
+let tryber = 'Fernanda';
+
+switch (tryber) {
+    case 'Caique':
+        console.log('Pessoa Especialista');
+        break;
+    case 'Fernanda':
+        console.log('Pessoa Instrutora');
+    default:
+        console.log('Essa pessoa não existe');
+        break;
+}
