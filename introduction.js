f0350c178262691721e46cb4a2247adb60405a03
Marcio <marciosoaresthoman@gmail.com>
2022-02-06 03:38:55 -0300

    Organizando/4.2

---
+++ introduction.js
@@ -0,0 +1,35 @@
+// Declaração do array
+
+let myArray = [10, 20, 30, 40, 50, 60];
+
+// Acessar o array - Saída
+
+
+// console.log(myArray);
+
+// Acessar o array individualmente
+
+// console.log(myArray[0]);
+// console.log(myArray[1]);
+// console.log(myArray[2]);
+// console.log(myArray[3]);
+// console.log(myArray[4]);
+// console.log(myArray[5]);
+
+
+// Quem é o FOR? (imagens)
+
+
+// inicializacao - condicao de parada - incremento ou decremento 
+for (let index = 0; index < myArray.length; index += 1 ) {
+   console.log(myArray[index]);
+}
+
+
+
+
+// O FOR nasceu para array? Vamos testar :)
+
+
+
+        
