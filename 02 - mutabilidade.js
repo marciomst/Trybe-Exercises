fa38cbd01e75d6d2bc1f57e96326818a6c5672dc
marcio soares thomann <marciosoaresthoman@gmail.com>
2022-03-03 23:16:58 -0300

    Resumo: let, const, arrow functions, template literals, operador ternário

---
+++ 02 - mutabilidade.js
@@ -0,0 +1,18 @@
+// const nome = 'Caique';
+
+// nome = 'Fernanda';
+
+
+// const array = [1, 2, 3];
+// array.push(4);
+// console.log(array);
+
+// const pessoa = {
+//   nome: 'Caique',
+//   idade: '40'
+// }
+
+
+// pessoa.nome = 'Fernanda';
+
+// console.log(pessoa);
---
+++ 03 - funcScope.js
@@ -0,0 +1,14 @@
+var x = 3;
+
+function func(nota) {
+  
+  if (nota >=7) {
+    let x = 48;
+    return x;
+  }
+  
+  return x;
+ }
+ 
+console.log(func(8)); 
+console.log(func(false)); 
