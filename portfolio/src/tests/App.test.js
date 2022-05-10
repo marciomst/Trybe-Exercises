350dafda2f4a5248087a2c3c57e00b5bb48d93f5
Marcio Soares Thomann <marciosoaresthoman@gmail.com>
2022-05-10 02:16:27 -0300

    Resumo: React RTL Testando Router

---
+++ portfolio/src/tests/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('Testando Nosso Portfolio', () => {
+
+});
